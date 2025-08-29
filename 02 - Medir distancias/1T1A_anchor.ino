a0d2955aea94a218cc58a8da94ec7dac5603ac82
= <=>
2025-08-29 21:57:33 +0200

    codigo comentado y ordenado.

--- 02 - Medir distancias/1T1A_anchor.ino
+++ 02 - Medir distancias/1T1A_anchor.ino
@@ -11,41 +11,33 @@ char anchor_addr[] = "84:00:5B:D5:A9:9A:E2:9C"; //#4
 // Antenna Delay: Sustituir con el valor obtenido en la calibracion
 uint16_t Adelay = 16580;
 
-// Valores obtenidos de calibraciones anteriores: 
-// 16635
-
-
+//Definiciones de Pines de la placa usada:
 #define SPI_SCK 18
 #define SPI_MISO 19
 #define SPI_MOSI 23
 #define DW_CS 4
 
-// connection pins
 const uint8_t PIN_RST = 27; // reset pin
 const uint8_t PIN_IRQ = 34; // irq pin
 const uint8_t PIN_SS = 4;   // spi select pin
 
-void setup()
-{
+void setup(){
+
   Serial.begin(115200);
   delay(1000); // Le doy 1 segundo para el monitor serie
 
-
   // Inicializo la configuración: 
 
-  // 1: Inicializo el bus SPI con los pines indicados antes.
+    // 1: Inicializo el bus SPI con los pines indicados antes.
   SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
 
-  // 2: Inicializo el DW1000
+    // 2: Inicializo el DW1000
   DW1000Ranging.initCommunication(PIN_RST, PIN_SS, PIN_IRQ); //Reset, CS, IRQ pin
 
 
   // Establezco el antenna delay indicado antes: 
   // Internamente, escribirá el valor que le paso en un registro concreto del DW1000
   DW1000.setAntennaDelay(Adelay);
-
-
-  // CALLBACKS: 
 
   DW1000Ranging.attachNewRange(newRange);
   //Esto es, básicamente: Cuando se termine una medición, lanza la función newRange.
@@ -93,19 +85,19 @@ void newRange()
   Serial.print(" m");
   Serial.print("\t RX power: ");
   Serial.println(DW1000Ranging.getDistantDevice()->getRXPower());
-
-
 }
 
-void newDevice(DW1000Device *device)
-{
+void newDevice(DW1000Device *device){
+
   // La librería DW1000 lanza este callback cuando detecta una comunicación desde un device con shortAdress distinta a las que ha visto hasta entonces.
   Serial.print("Device added: ");
   Serial.println(device->getShortAddress(), HEX);
 }
 
-void inactiveDevice(DW1000Device *device)
-{
+void inactiveDevice(DW1000Device *device){
+
+  //Dentro de DWdevice.h, se define el inactivity_time como 1s.
+  // Si pasa ese tiempo sin señal de un dispositivo que ya había registrado antes, lo considero inactivo. 
   Serial.print("Delete inactive device: ");
   Serial.println(device->getShortAddress(), HEX);
 }
