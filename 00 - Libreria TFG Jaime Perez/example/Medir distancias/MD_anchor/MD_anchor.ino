04a466e452dabcf27757d71ac4bf47c8cc1b1641
= <=>
2025-09-02 18:37:32 +0200

    publicación librería con archivos editables

---
+++ 00 - Libreria TFG Jaime Perez/example/Medir distancias/MD_anchor/MD_anchor.ino
@@ -0,0 +1,94 @@
+//1 Tag y 1 Anchor: 
+// ANCHOR SETUP
+
+#include <SPI.h>
+#include "DW1000Ranging.h"
+#include "DW1000.h"
+
+// Los 2 bytes de la izquierda son la short address.
+// NOMENCLATURA: A para Anchors, B para Tags
+char anchor_addr[] = "A1:00:5B:D5:A9:9A:E2:9C"; 
+
+// Antenna Delay: Sustituir con el valor obtenido en la calibracion
+uint16_t Adelay = 16580;
+
+//Definiciones de Pines de la placa usada:
+#define SPI_SCK 18
+#define SPI_MISO 19
+#define SPI_MOSI 23
+#define DW_CS 4
+
+const uint8_t PIN_RST = 27; // reset pin
+const uint8_t PIN_IRQ = 34; // irq pin
+const uint8_t PIN_SS = 4;   // spi select pin
+
+void setup(){
+
+  Serial.begin(115200);
+  delay(1000); // Le doy 1 segundo para el monitor serie
+
+  // Inicializo la configuración: 
+
+    // 1: Inicializo el bus SPI con los pines indicados antes.
+  SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
+
+    // 2: Inicializo el DW1000
+  DW1000Ranging.initCommunication(PIN_RST, PIN_SS, PIN_IRQ); //Reset, CS, IRQ pin
+
+
+  // Establezco el antenna delay indicado antes: 
+  // Internamente, escribirá el valor que le paso en un registro concreto del DW1000
+  DW1000.setAntennaDelay(Adelay);
+
+  DW1000Ranging.attachNewRange(newRange);
+  //Esto es, básicamente: Cuando se termine una medición, lanza la función newRange.
+  DW1000Ranging.attachNewDevice(newDevice);
+  DW1000Ranging.attachInactiveDevice(inactiveDevice);
+
+  //inicio la placa como ANCHOR. los parámetros son: 
+  // dirección - modo de funcionamiento - ¿Tomar dirección aleatoria? 
+  // La dirección aleatoria sirve para pruebas "rapidas". Así evito tener que estar cambiando todas las direcciones.
+  
+  DW1000Ranging.startAsAnchor(anchor_addr, DW1000.MODE_LONGDATA_RANGE_LOWPOWER, false);
+  // DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_SHORTDATA_FAST_LOWPOWER);
+  // DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_LONGDATA_FAST_LOWPOWER);
+  // DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_SHORTDATA_FAST_ACCURACY);
+  // DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_LONGDATA_FAST_ACCURACY);
+  // DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_LONGDATA_RANGE_ACCURACY);
+}
+
+void loop()
+{
+  DW1000Ranging.loop();
+}
+
+void newRange()
+{
+
+  //Muestro desde qué dispositivo se ha hecho el range
+  Serial.print(" Desde: ");
+  Serial.print(DW1000Ranging.getDistantDevice()->getShortAddress(), HEX);
+  // La función getDistantDevice() devuelve el objeto "device" con el que se ha hecho ranging. 
+  // A este device puedo pedirle métodos como: getShortAddress, getAddress, getRange, etc.
+
+  Serial.print("\t Distancia: ");
+  Serial.print(DW1000Ranging.getDistantDevice()->getRange());
+  Serial.print(" m");
+  Serial.print("\t RX power: ");
+  Serial.println(DW1000Ranging.getDistantDevice()->getRXPower());
+}
+
+void newDevice(DW1000Device *device){
+
+  // La librería DW1000 lanza este callback cuando detecta una comunicación desde un device con shortAdress distinta a las que ha visto hasta entonces.
+  Serial.print("Device added: ");
+  Serial.println(device->getShortAddress(), HEX);
+}
+
+void inactiveDevice(DW1000Device *device){
+
+  //Dentro de DWdevice.h, se define el inactivity_time como 1s.
+  // Si pasa ese tiempo sin señal de un dispositivo que ya había registrado antes, lo considero inactivo. 
+  Serial.print("Delete inactive device: ");
+  Serial.println(device->getShortAddress(), HEX);
+}
---
+++ 00 - Libreria TFG Jaime Perez/example/Medir distancias/MD_tag/MD_tag.ino
@@ -0,0 +1,70 @@
+// 1 TAG - 1 ANCHOR
+// TAG
+
+#include <SPI.h>
+#include "DW1000Ranging.h"
+#include "DW1000.h"
+
+#define SPI_SCK 18
+#define SPI_MISO 19
+#define SPI_MOSI 23
+#define DW_CS 4
+
+// connection pins
+const uint8_t PIN_RST = 27; // reset pin
+const uint8_t PIN_IRQ = 34; // irq pin
+const uint8_t PIN_SS = 4;   // spi select pin
+
+// TAG antenna delay defaults to 16384
+
+// Los 2 bytes de la izquierda son la short address.
+// NOMENCLATURA: A para Anchors, B para Tags
+char tag_addr[] = "B1:00:22:EA:82:60:3B:9C";
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(1000);
+
+  //init the configuration
+  SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
+  DW1000Ranging.initCommunication(PIN_RST, PIN_SS, PIN_IRQ); //Reset, CS, IRQ pin
+
+  DW1000Ranging.attachNewRange(newRange);
+  DW1000Ranging.attachNewDevice(newDevice);
+  DW1000Ranging.attachInactiveDevice(inactiveDevice);
+
+// start as tag, do not assign random short address
+
+  DW1000Ranging.startAsTag(tag_addr, DW1000.MODE_LONGDATA_RANGE_LOWPOWER, false);
+}
+
+void loop()
+{
+  DW1000Ranging.loop();
+}
+
+void newRange()
+{
+  Serial.print("Desde: ");
+  Serial.print(DW1000Ranging.getDistantDevice()->getShortAddress(), HEX);
+  Serial.print("\t Distancia: ");
+  Serial.print(DW1000Ranging.getDistantDevice()->getRange());
+  Serial.print(" m");
+  Serial.print("\t RX power: ");
+  Serial.print(DW1000Ranging.getDistantDevice()->getRXPower());
+  Serial.println(" dBm");
+
+}
+
+void newDevice(DW1000Device *device)
+{
+  Serial.print("Device added: ");
+  Serial.println(device->getShortAddress(), HEX);
+}
+
+void inactiveDevice(DW1000Device *device)
+{
+  Serial.print("delete inactive device: ");
+  Serial.println(device->getShortAddress(), HEX);
+}
