0df2da2270f17ae5ac9b0ecd86d5529e7b7b15b1
= <=>
2025-09-04 09:26:39 +0200

    añadido archivo de tag para centralizar datos

--- 04 - Centralizar Datos/CD_tag.ino
+++ 04 - Centralizar Datos/CD_tag.ino
@@ -1,4 +1,40 @@
-/* DATOS CENTRALIZADOS EN UN ANCHOR
-    El tag se limitará a responder a las comunicaciones que le lleguen. Actuará como actuaban los anchors en los ejemplos anteriores vistos en el proyecto. Por tanto, el principal cambio hecho es la configuración del tag.
-*/
+// Centralizar datos
+// TAG (Actuará como respondedor únicamente)
 
+#include <SPI.h>
+#include "DW1000Ranging.h"
+#include "DW1000.h"
+
+#define SPI_SCK 18
+#define SPI_MISO 19
+#define SPI_MOSI 23
+#define DW_CS 4
+
+// connection pins
+const uint8_t PIN_RST = 27; // reset pin
+const uint8_t PIN_IRQ = 34; // irq pin
+const uint8_t PIN_SS = 4;   // spi select pin
+
+// TAG antenna delay defaults to 16384
+
+// Los 2 bytes de la izquierda son la short address.
+// NOMENCLATURA: A para Anchors, B para Tags
+#define DEVICE_ADDR "B1:00:22:EA:82:60:3B:9C"
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(1000);
+
+  //init the configuration
+  SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
+  DW1000Ranging.initCommunication(PIN_RST, PIN_SS, PIN_IRQ); //Reset, CS, IRQ pin
+
+  // start as tag, do not assign random short address
+  DW1000Ranging.startAsResponder(DEVICE_ADDR, DW1000.MODE_LONGDATA_RANGE_LOWPOWER, false);
+}
+
+void loop(){
+
+  DW1000Ranging.loop();
+}
