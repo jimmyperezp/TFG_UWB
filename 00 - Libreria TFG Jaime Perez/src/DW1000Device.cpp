45fa3ec243b02f0146d7de2ff839d51f7cc42752
Jimmy <jaimeperezzp@gmail.com>
2025-10-14 15:55:59 +0200

    eliminados comentarios iniciales

--- 00 - Libreria TFG Jaime Perez/src/DW1000Device.cpp
+++ 00 - Libreria TFG Jaime Perez/src/DW1000Device.cpp
@@ -1,25 +1,4 @@
-/*
- * Copyright (c) 2015 by Thomas Trojer <thomas@trojer.net> and Leopold Sayous <leosayous@gmail.com>
- * Decawave DW1000 library for arduino.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * @file DW1000Device.cpp
- * Arduino global library (source file) working with the DW1000 library
- * for the Decawave DW1000 UWB transceiver IC.
- * 
- * @todo complete this class
- */
+
 
 #include "DW1000Device.h"
 #include "DW1000.h"
@@ -48,6 +27,7 @@ DW1000Device::DW1000Device(byte deviceAddress[], byte shortAddress[]) {
 	//we set the 2 bytes address
 	setShortAddress(shortAddress);
 }
+
 
 DW1000Device::~DW1000Device() {
 }
